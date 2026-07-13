0ce2639f21a01d0aaa794df6b3957732a22fde59
Muhammad Robiansyah <muhammadrobiansyah015@gmail.com>
2026-07-13 14:30:00 +0700

    Tambah konfigurasi aplikasi

---
+++ config/checksheet_gsheets.php
@@ -0,0 +1,185 @@
+<?php
+
+return array_merge([
+    'webapp_url' => env('GSHEET_COPY_WEBAPP_URL'),
+    'secret' => env('GSHEET_COPY_SECRET', ''),
+], array (
+  'disassembly_templates' => 
+  array (
+    'Engine' => 
+    array (
+      'WA800-3' => '1zNdwCw65dVaO0zrdX7ttFmGetjWLXKZcApt55yl5iuY',
+      'GD825A-2' => '1FE5c4TiOVPJYvScR_YykmEVvSeHwvB2EcpR0QvMjHrg',
+      'GD825A' => '1FE5c4TiOVPJYvScR_YykmEVvSeHwvB2EcpR0QvMjHrg',
+      'D155-6' => '1YNCACISXD1rspgnI2-zLi1EQi0a4SU4qG9vYXSe7BLI',
+      'D155' => '1YNCACISXD1rspgnI2-zLi1EQi0a4SU4qG9vYXSe7BLI',
+      'D375-6' => '1LVny6gUcwFpcxpzTJc4GR1kWqAwumE8a-AzBqNWN-9A',
+      'D375' => '1LVny6gUcwFpcxpzTJc4GR1kWqAwumE8a-AzBqNWN-9A',
+      'PC1250-8' => '1LVny6gUcwFpcxpzTJc4GR1kWqAwumE8a-AzBqNWN-9A',
+      'PC2000-8' => '1kIjBP4R4MWPkpFzXIU7Smcwnyy2DoR2Pzj2oggmn3tY',
+    ),
+    'Control Valve' => 
+    array (
+      'PC1250-8' => '1iqb7_rZwRxy3BHl863jbZ7utF8S1t-OOkrvsk4deApc',
+      'PC2000-8' => '1K8NmjKQhLBHdeFQfDzrYuFgxTBSVfguMDarZ6nxG1lE',
+      'D155-6' => '1L0tXHGG1zWAr7M-f71HnouZiZHa5VzXb4BEfnIHZMk4',
+      'D375-6' => '12V3ZPBH_haLY4muDd-MRU74_h3YIBRv2zpXXrjCtBIg',
+      'HD785-7' => '1n0yQLLM9e6YXW7-tkfag6WTsG5It08ivJd5uJljp-Ao',
+      'GD825A-2' => '1xnII3Dj2Wa3sIXbAluQtbhlAe5wH3ATEgTUC2lhkn6M',
+      'GD825A' => '1xnII3Dj2Wa3sIXbAluQtbhlAe5wH3ATEgTUC2lhkn6M',
+      'WA800-3' => '1M701TMirI6he5nkteGjYRso6bIfCWTWMQmpZ8dG793M',
+    ),
+    'Hydraulic Cylinder' => 
+    array (
+      'HD785-7' => '18DT3AEC-HCV7LZzK0iNIgewULatkn_VES7W4E5-eO7I',
+    ),
+    'Front Suspension' => 
+    array (
+      'HD785-7' => '1m_24Zk8-QlSKyNY7Mu0a-YrTuzAR0hcOq1B2LvE2C4s',
+    ),
+    'Rear Suspension' => 
+    array (
+      'HD785-7' => '1wtfzaxmFLBlFOb8m7NPFLipW6zB1b79oK2YGsD0XKOg',
+    ),
+  ),
+  'measurement_templates' => 
+  array (
+    'Engine' => 
+    array (
+      'WA800-3' => '16YMihLgPAjpcBZDlmd6DtxocNXJPsPrIpjspZVhduCI',
+      'GD825A-2' => '1Xnmcb6EmX5mCGRvuSfp15TxPNZJnxMfnmnczZkFWDlU',
+      'GD825A' => '1Xnmcb6EmX5mCGRvuSfp15TxPNZJnxMfnmnczZkFWDlU',
+      'D155-6' => '1zRB8oc1KxJM7tbKr0ri6Rl1dAugwdazHh8ky6A27OZM',
+      'D155' => '1zRB8oc1KxJM7tbKr0ri6Rl1dAugwdazHh8ky6A27OZM',
+      'D375-6' => '1wIxlv5YAhJo90cnBGwSBHqfAMqDa77zgIwQqOeQ2a44',
+      'D375' => '1wIxlv5YAhJo90cnBGwSBHqfAMqDa77zgIwQqOeQ2a44',
+      'PC1250-8' => '1wIxlv5YAhJo90cnBGwSBHqfAMqDa77zgIwQqOeQ2a44',
+      'PC2000-8' => '1fB7lNhIQDDUi-Kb3zqs3QJxzTDPsEqLyQGkP4ilGYqI',
+    ),
+    'Control Valve' => 
+    array (
+      'PC1250-8' => '1JG9fH3QTLa9PNQDa4u7RM5PCG_AwBL8oKUAbcJpkxXU',
+      'PC2000-8' => '1bVLDjFwf9JgQ1MuvpE5SfJF0BN-2Rf5DZ60NnmQk5l4',
+      'D155-6' => '1Eo44gTSwJ9gaQzM5iKJe_Trgl34MBxUpOSijjGfUmVo',
+      'D375-6' => '1gFzsngHNgJqHu9vdOm_62C886AJX5uR5_bGS6tkA07E',
+      'HD785-7' => '1iSXSL-TldmyzTdqM-zEBZ615Naxq0ROe7g73AdI3D-4',
+      'GD825A-2' => '1dDt7wM9_auvAu8XlXpEzB2x83nSAnVC3rYlRkyDYYr8',
+      'GD825A' => '1dDt7wM9_auvAu8XlXpEzB2x83nSAnVC3rYlRkyDYYr8',
+      'WA800-3' => '1i4EF4iqFS4FjIQ6B1QIDj1Iylhje8GuXn-zePAC6sic',
+    ),
+    'Hydraulic Cylinder' => 
+    array (
+      'HD785-7' => '13fJKSbO6n3XplniCOBfM6Wug6ezrrbTQeOFtxAA0hcQ',
+    ),
+    'Front Suspension' => 
+    array (
+      'HD785-7' => '1Doa1A1veZjJDJw9w9snZ5EMAH3KkQvkJW-OxhWvV5PY',
+    ),
+    'Rear Suspension' => 
+    array (
+      'HD785-7' => '1htpxUh8X9Z-EYOjNILvaZCMAXnOVVaK253NePti-MzA',
+    ),
+  ),
+  'subassy_disassembly_templates' => 
+  array (
+    'Engine' => 
+    array (
+      'WA800-3' => '1a5KaeWzZtMVQXQVadDBKBOLc-KDhic-1mVXnJb93-34',
+      'GD825A-2' => '1RbE5UMcVKK3gKwisEIkFVzpCq6mmLwjCPQVSdBE8mLo',
+      'GD825A' => '1RbE5UMcVKK3gKwisEIkFVzpCq6mmLwjCPQVSdBE8mLo',
+      'D155-6' => '1pTpQSLileQzBYoQ1BhO1hAH199-uP8EZ_f71j7M5rBQ',
+      'D155' => '1pTpQSLileQzBYoQ1BhO1hAH199-uP8EZ_f71j7M5rBQ',
+      'D375-6' => '1uHSrcdykBcn8TNOVMjcmPDyS7scA5bKfqVfmAaWZ_G0',
+      'D375' => '1uHSrcdykBcn8TNOVMjcmPDyS7scA5bKfqVfmAaWZ_G0',
+      'PC1250-8' => '1uHSrcdykBcn8TNOVMjcmPDyS7scA5bKfqVfmAaWZ_G0',
+      'PC2000-8' => '1t4ylw-hZViirCqjbqHtTVwggoC5x2s6BAUib-pokVEY',
+    ),
+  ),
+  'subassy_measurement_templates' => 
+  array (
+    'Engine' => 
+    array (
+      'WA800-3' => '16lGJ-wrqewzNlwtNVYcBU2B7tRi3UqcMzOXixP0I8do',
+      'GD825A-2' => '1JzQfh5yFWJekOiwZo7osMrfGUy_immS6CoMxPcMhLlc',
+      'GD825A' => '1JzQfh5yFWJekOiwZo7osMrfGUy_immS6CoMxPcMhLlc',
+      'D155-6' => '17K64i-8cBtcFdkTMLK3VDsZRLxVipSUJFZd9_90rCSw',
+      'D155' => '17K64i-8cBtcFdkTMLK3VDsZRLxVipSUJFZd9_90rCSw',
+      'D375-6' => '13PkmzN45Hl--GFSkwYh4rxndAeJj8Zu9XAa0C2OYx5w',
+      'D375' => '13PkmzN45Hl--GFSkwYh4rxndAeJj8Zu9XAa0C2OYx5w',
+      'PC1250-8' => '13PkmzN45Hl--GFSkwYh4rxndAeJj8Zu9XAa0C2OYx5w',
+      'PC2000-8' => '1-3tBQhnbKSy0qy-GLhTcXRs9XBA1X0gvKrB_sNz-pM8',
+    ),
+  ),
+  'sdr_templates' => 
+  array (
+    'default' => '1HvxiqXGEvH_nscYugPjOEfgIdq9Ps9nEyKqt_vNrd_8',
+  ),
+  'assembly_templates' => 
+  array (
+    'Engine' => 
+    array (
+      'SA12V140E-1' => '1VQ-Z7g_kR9XXApRWbjG7lzC0zCzHnVquyxjzEyHG12w',
+      'SAA6D140E-5' => '1iAEDa_gYTGXGIG8Gn5IKadQCRtSexZFUpV4D0o4KMiE',
+      'D155-6' => '1iAEDa_gYTGXGIG8Gn5IKadQCRtSexZFUpV4D0o4KMiE',
+      'D155' => '1iAEDa_gYTGXGIG8Gn5IKadQCRtSexZFUpV4D0o4KMiE',
+      'SAA6D170E-5' => '1dTEG8f4vRWa4tZMyVrpld47C4GtpAO-qELz2ZVh_UQU',
+      'D375-6' => '1dTEG8f4vRWa4tZMyVrpld47C4GtpAO-qELz2ZVh_UQU',
+      'D375' => '1dTEG8f4vRWa4tZMyVrpld47C4GtpAO-qELz2ZVh_UQU',
+      'PC1250-8' => '1dTEG8f4vRWa4tZMyVrpld47C4GtpAO-qELz2ZVh_UQU',
+      'WA800-3' => '1VQ-Z7g_kR9XXApRWbjG7lzC0zCzHnVquyxjzEyHG12w',
+      'SA6D140E-2' => '11bff8oj1i6rZvF4fILZgzdwSM-B7JShoPO2hfCzoKYU',
+      'GD825A-2' => '11bff8oj1i6rZvF4fILZgzdwSM-B7JShoPO2hfCzoKYU',
+      'GD825A' => '11bff8oj1i6rZvF4fILZgzdwSM-B7JShoPO2hfCzoKYU',
+      'SAA12V140E-3' => '1x_rSXV1Dmafy8vGE7SFTsA5OE-ikmaj2XUMC7Z5sXVw',
+      'PC2000-8' => '1x_rSXV1Dmafy8vGE7SFTsA5OE-ikmaj2XUMC7Z5sXVw',
+    ),
+    'Control Valve' => 
+    array (
+      'D155-6' => '1ZDzQNsQBX6y36wu3Ck6D5E0tbw_l_bmoAcUq90tyN8o',
+      'D375-6' => '1VGk-KzKNjkvUQSfbutpB8YOjFVQFTqpcyO96n7tWNFo',
+      'GD825A-2' => '1M-xw5HgTObFou2elj4rZmrQDSRuoi4TzG1_T7dGH1AI',
+      'GD825A' => '1M-xw5HgTObFou2elj4rZmrQDSRuoi4TzG1_T7dGH1AI',
+      'HD785-7' => '1xun23KqoNoow1rikIRgpxQKaxr8x1XXDSWz9ygNISIY',
+      'PC2000-8' => '1X68SmoetAaGr0qIJQIlev1PvuxMZu3XfGhUBjpVnbDk',
+      'WA800-3' => '1nBkJpknzQiTWQ6VRxKLB7yhsHKfuGEbtqvnL54dDIZc',
+      'PC1250-8' => '1K1nXDJglsnfCyvtp48RuOJamHC2Ya25ZpR3Ct1Aqmas',
+    ),
+    'Hydraulic Cylinder' => 
+    array (
+      'HD785-7' => '1rXHCffDwdkkPhfo4lnExzTgbw7YxPHA4bYZuWVvnEX0',
+    ),
+    'Rear Suspension' => 
+    array (
+      'HD785-7' => '1k9MbdRNp2Camg3Zz5tIUwdfr42BBKxwCw1LNYhP2WRM',
+    ),
+    'Front Suspension' => 
+    array (
+      'HD785-7' => '1a0imVP_g356peC72e_JUfoDR2SguO3HSgMQjUpNAq-Q',
+    ),
+  ),
+  'testbench_templates' => 
+  array (
+    'Engine' => 
+    array (
+      'SA12V140E-1' => '1BH9RDpm_tHnxTQgcrws8WxAuBDHxzycuMIbdrvnF9YM',
+      'SAA12V140E-3' => '1MkGgaHR9AEi3XmL5ZUejqLDCQgDI5wLJumnaIoc19Es',
+      'PC2000-8' => '1MkGgaHR9AEi3XmL5ZUejqLDCQgDI5wLJumnaIoc19Es',
+      'SAA6D140E-5' => '11NWOmZaxn53oUB1XYYifAw8QR-ewM1DiWLrzjFYOe9o',
+      'D155-6' => '11NWOmZaxn53oUB1XYYifAw8QR-ewM1DiWLrzjFYOe9o',
+      'D155' => '11NWOmZaxn53oUB1XYYifAw8QR-ewM1DiWLrzjFYOe9o',
+      'SAA6D170E-5' => '1Qh-3Sw6DyJbePO_ITg_hf7EpAQt5Nt5p4OtUmiktVwI',
+      'D375-6' => '1Qh-3Sw6DyJbePO_ITg_hf7EpAQt5Nt5p4OtUmiktVwI',
+      'D375' => '1Qh-3Sw6DyJbePO_ITg_hf7EpAQt5Nt5p4OtUmiktVwI',
+      'PC1250-8' => '1Qh-3Sw6DyJbePO_ITg_hf7EpAQt5Nt5p4OtUmiktVwI',
+      'WA800-3' => '1BH9RDpm_tHnxTQgcrws8WxAuBDHxzycuMIbdrvnF9YM',
+      'SA6D140E-2' => '1Bq_Se0YHTMVnHZ9o8ST-sXhnnlnUC48UfRSSx_ut4a8',
+      'GD825A-2' => '1Bq_Se0YHTMVnHZ9o8ST-sXhnnlnUC48UfRSSx_ut4a8',
+      'GD825A' => '1Bq_Se0YHTMVnHZ9o8ST-sXhnnlnUC48UfRSSx_ut4a8',
+    ),
+    'Control Valve' => 
+    array (
+      'PC1250-8' => '17XgcOvcFdu57vGpJRnayvhXXaP1PrdM7MWeVvsEdKdo',
+      'PC2000-8' => '1pFiexqFEVQdiXIYLRJxpX_tWYU10ml7DexrN7_JAcXw',
+    ),
+  ),
+));
